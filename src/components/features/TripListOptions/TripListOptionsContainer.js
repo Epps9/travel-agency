578899a8bdf88a037dc6b96a1287b76abbb48aa1
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-09-25 13:49:16 +0200

    Add filters

--- src/components/features/TripListOptions/TripListOptionsContainer.js
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -1,7 +1,8 @@
 import {connect} from 'react-redux';
 import TripListOptions from './TripListOptions';
 import {getAllTags} from '../../../redux/tagsRedux';
-import {getAllFilters, changeSearchPhrase} from '../../../redux/filtersRedux';
+import {getAllFilters, changeSearchPhrase, addTag} from '../../../redux/filtersRedux';
+
 
 const mapStateToProps = state => ({
   tags: getAllTags(state),
@@ -10,6 +11,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
+  tags: tag => dispatch(addTag(tag)),
   // TODO - add more dispatchers for other filters
 });
 
