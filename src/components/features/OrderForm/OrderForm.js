7f16346d833d3adc354561a1851017ca64227d0b
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-10-11 13:21:31 +0200

    Server problem

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -6,6 +6,39 @@ import OrderSummary from '../../features/OrderSummary/OrderSummary';
 import {Row, Col} from 'react-flexbox-grid';
 import OrderOption from '../OrderOption/OrderOption';
 
+import Button from '../../common/Button/Button';
+
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
 const OrderForm = ({tripCost, options, setOrderOption}) => (
   <Row>
     {pricing.map(option => (
@@ -16,7 +49,9 @@ const OrderForm = ({tripCost, options, setOrderOption}) => (
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>
+    <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
   </Row>
+  
 );
 
 OrderForm.propTypes = {
